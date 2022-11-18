5483ebf7ad4549840cdc6e3ae571bb287cad03bf
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-11-18 15:59:21 -0300

    study generic class

---
+++ generic.kt
@@ -0,0 +1,32 @@
+//Generics da flexibilidade na tipagem das informações
+//O generic é passando entre <> e chamado no parametro
+class MutableStack <e>(vararg items:e){
+
+    private val elements = items.toMutableList() //Converte os elementos do construtuor para uma lista
+
+    fun push(element: e) = elements.add(element) //Adiciona o elemento 
+
+    fun peek(): e = elements.last() //Pega o ultimo elemento
+
+    fun pop(): e = elements.removeAt(elements.size -1) //Remove o último elemento
+
+    // fun is_empty() = elements.is_empty() //Verifica se está vazia
+
+    fun size() = elements.size //tamanho da lista
+
+    override fun toString() = "MutableStack (${elements.joinToString()})" //Convertendo a lista para texto
+}
+
+fun main(){
+    val stack = MutableStack("A","B","C","D")
+
+    stack.push("E")
+    println(stack) //Aqui é chamada a função to string
+
+    println("peek(): ${stack.peek()}")
+    
+    for(i in 1..stack.size()){ //Roda no tamanho da lista e vai retirando o ultimo termo
+        println("pop(): ${stack.pop()}")
+        println(stack)
+    }
+}
