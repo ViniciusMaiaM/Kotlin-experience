af00c3f1c7e359da8130133edea1fd2a11a25185
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-11-22 07:27:21 -0300

    add coments

--- poo/simple_heritage.kt
+++ poo/simple_heritage.kt
@@ -1,11 +1,12 @@
-open class Dog{
-    open fun say_hi(){
+open class Dog{ //A classe open quer dizer que ela pode ser utilizada por outras classes, como extensão
+    open fun say_hi(){ //Mesma coisa da classe
         println("Wow wow")
     }
 }
 
-class Terrier : Dog(){
-    override fun say_hi(){
+class Terrier : Dog(){ // : é a extensão da classe, nesse caso, terrier é um cachorro, 
+    // assim herda todas as caracterísitcas e comportamentos
+    override fun say_hi(){ //Já que say_hi é open podemos sobscrevela
         println("wif wif")
     }
 }
